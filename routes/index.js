ac9fa1e612ba0af954f78e634c6bde277df549f4
Vincent Iacullo <iacullovincent@gmail.com>
2026-02-20 18:48:35 -0600

    generated express application using express generator

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET users listing. */
+router.get('/users', (req, res, next) => {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
