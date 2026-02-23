82f0458ebb1412b9dfbe3b900748f4997c68d260
Vincent Iacullo <iacullovincent@gmail.com>
2026-02-23 17:55:53 -0600

    Incorporated EJS into main

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
 });
 
 /* GET users listing. */
-router.get('/users', (req, res, next) => {
-  res.send('respond with a resource');
+router.get("/users", (req, res, next) => {
+  res.send("respond with a resource");
 });
 
+router.get("/quotes", (req, res, next) => {
+  res.render("quote", {
+    quotes: [
+      {
+        id: "5",
+        description: "Heater tuneup",
+        price: 240,
+        associate: "John Doe",
+        status: "Open",
+      },
+      {
+        id: "6",
+        description: "Furnace removal",
+        price: 240,
+        associate: "Dave",
+        status: "Open",
+      },
+      {
+        id: "6",
+        description: "Furnace removal",
+        price: 240,
+        associate: "Dave",
+        status: "Open",
+      },
+      {
+        id: "6",
+        description: "Furnace removal",
+        price: 240,
+        associate: "Dave",
+        status: "Open",
+      },
+    ],
+  });
+});
 module.exports = router;
